a72ccf88f828b8b6b934470a2cc10d1f00c4389e
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2024-01-09 19:20:42 +0100

    Ajoutez des fichiers projet.

---
+++ Data/LibraryClientContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LibraryClient.Models;
+
+namespace LibraryClient.Data
+{
+    public class LibraryClientContext : DbContext
+    {
+        public LibraryClientContext (DbContextOptions<LibraryClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LibraryClient.Models.Livre> Livres { get; set; } = default!;
+        public DbSet<LibraryClient.Models.Auteur> Auteurs { get; set; } = default!;
+        //public LibraryClientContext() { }
+    }
+}
---
+++ Models/Emprunt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryClient.Models
+{
+    public class Emprunt
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        
+        [ForeignKey("LivreId")]
+        public Livre Livre { get; set; }
+        public int LivreId { get; set; }
+
+        [ForeignKey("AdherentId")]
+        public Adherent Adherent { get; set; }
+        public int AdherentId { get; set; }
+
+        public DateTime Date_empr {  get; set; }
+        public DateTime Date_retour { get; set; }
+        public float Montant { get; set; }
+        public bool EtatRetour { get; set; }
+
+        
+
+        public Emprunt() { }
+
+        public Emprunt(Livre livre, Adherent adherent, DateTime date_empr, DateTime date_retour, float montant, bool etatRetour)
+        {
+            this.Livre=livre; this.Adherent=adherent;
+            this.Date_empr = date_empr;
+            this.Date_retour = date_retour;
+            this.Montant = montant;
+            this.EtatRetour = etatRetour;
+        }
+
+    }
+}
